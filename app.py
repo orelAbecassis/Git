23f7a84570d89f5d9831d66c138643badc530831
Orel ABK6 <orel.abecassis@ortmontreuil.fr>
2024-07-02 00:25:57 +0200

    create connexion bdd

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+# connexion a la bdd
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
+
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3002)
+    app.run(host='0.0.0.0', port=5000)
